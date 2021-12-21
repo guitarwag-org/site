90b720e27d9a71fca0733d4c767846a741087b15
Wagner Jose da Silva <wagner.silva@lagoasoft.com>
2021-12-20 21:03:51 -0300

    stripe

--- src/components/Stripe.jsx
+++ src/components/Stripe.jsx
@@ -1,43 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {loadStripe} from '@stripe/stripe-js';
-import {
-    CardElement,
-    Elements,
-    useStripe,
-    useElements,
-} from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51K88XyCrmjniYqrtczFGpUjWpiJ5SqdYFylbgxYndJcGiTDeppDg88RU2mwrQMG2CHBWvXmi0z4wWjmb6k7XuK2N00lXucjGrs');
 
 const CheckoutForm = () => {
-    const stripe = useStripe();
-    const elements = useElements();
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        if (elements == null) {
-            return;
+    const [loading, setLoading] = useState(false);
+    const [err, setError] = useState(null);
+    const handleSubmit = async () => {
+        setLoading(true);
+        try {
+            const stripe = await stripePromise;
+            const { error } = await stripe.redirectToCheckout({
+                    lineItems: [
+                        {
+                            price: 'price_1K8vbwCrmjniYqrtUX2sd8Ny',
+                            quantity: 1
+                        },
+                        {
+                            price: 'price_1K8vcQCrmjniYqrtkFnYO5W4',
+                            quantity: 1
+                        }
+                    ],
+                    mode: "payment",
+                    cancelUrl: window.location.origin,
+                    successUrl: `${window.location.origin}/success?id=298347982374298372`,
+                    customerEmail: 'wagner@gmail.com'
+                }
+            );
+            setError(error)
+            setLoading(false)
+        } catch (e) {
+            setError(e)
+            setLoading(false)
         }
 
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-            type: 'card',
-            card: elements.getElement(CardElement),
-        });
+
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <CardElement />
-            <button type="submit" disabled={!stripe || !elements}>
+            {err && <p className="text-red-700">{err.message}</p>}
+            <button type="button" disabled={loading} onClick={handleSubmit} className="bg-gray-300 p-3 hover:bg-gray-400 mt-4 text-black">
                 Pay
             </button>
         </form>
     );
 };
 
-const stripePromise = loadStripe('pk_test_51K88XyCrmjniYqrtczFGpUjWpiJ5SqdYFylbgxYndJcGiTDeppDg88RU2mwrQMG2CHBWvXmi0z4wWjmb6k7XuK2N00lXucjGrs');
 
 export const Stripe = () => (
-    <Elements stripe={stripePromise}>
         <CheckoutForm />
-    </Elements>
 );
